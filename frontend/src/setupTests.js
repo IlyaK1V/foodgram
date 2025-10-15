8f4dfb3ec8183a9bf525e31330ab79cea3cfa9cb
Ilya Kravchenko <ilyak475@gmail.com>
2025-10-15 20:11:30 +0300

    fix npm

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,4 +2,13 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// src/setupTests.js
 import '@testing-library/jest-dom';
+
+// 👇 Полифилл для Node.js 21+
+if (typeof global.clearImmediate === 'undefined') {
+  global.clearImmediate = (id) => clearTimeout(id);
+}
+if (typeof global.setImmediate === 'undefined') {
+  global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
+}
